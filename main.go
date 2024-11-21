0bf69eeffd770b31194dd65a4263600be29459fa
Christoffer Back <backchristoffer@hotmail.com>
2024-11-21 17:06:42 +0100

    merge issue with boilerplate http GET code

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
-
 import (
 	"fmt"
+	"io"
+	"net/http"
 )
-
 func main() {
-	fmt.Println("boilerplate")
-}
+	resp, err := http.Get("https://access.redhat.com/hydra/rest/securitydata/cve.json?product=OpenShift%20Container%20Platform&severity=important")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s", body)
